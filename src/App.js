c8027a7d8cb1c1dca5c60f6548cc9f6edf0889da
anjaneyamustoor <anjaneyamustoor167@gmail.com>
2022-08-31 21:51:20 +0530

    Change button text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   }
   return (
     <div className="App">
-      <button onClick={signInWithGoogle}>Sign in with Google</button>
+      <button onClick={signInWithGoogle}>Google </button>
     </div>
   );
 }
